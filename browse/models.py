1ab537af8e880b00bb38cad054a8496743b9ded2
liushuaikobe <liushuaikobe@gmail.com>
2013-03-16 21:48:48 +0800

    complete the DB designing initially

--- browse/models.py
+++ browse/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class kobe_posts(models.Model):
+	post_title = models.CharField(max_length = 200)
+	post_date = models.DateTimeField(auto_now = True)
+	post_view_num = models.IntegerField()
+	post_content = models.TextField()
+	post_type = models.ForeignKey('kobe_type')
+	post_cate = models.ForeignKey('kobe_category')
+
+	def __unicode__(self):
+		return self.post_title
+
+class kobe_type(models.Model):
+	'''translate,reserved,Original'''
+	type_name = models.CharField(max_length = 100)
+
+	def __unicode__(self):
+		return self.type_name
+
+class kobe_category(models.Model):
+	'''C++,Python,Java'''
+	cate_name = models.CharField(max_length = 100)
+
+	def __unicode__(self):
+		return cate_name
+
+class kobe_admin(models.Model):
+	user_email = models.EmailField()
+	user_nicename = models.CharField(max_length = 50)
+	user_registered = models.DateTimeField(auto_now = True)
+	user_pass = models.CharField(max_length = 70)
+
+	def __unicode__(self):
+		return user_nicename
