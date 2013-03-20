1751783efd73106ec6235245a7507a79530b418d
liushuaikobe <liushuaikobe@gmail.com>
2013-03-20 16:46:03 +0800

    add category management to admin page

--- manage/views.py
+++ manage/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def manage(request):
-	return render_to_response('admin_settings.html',context_instance = RequestContext(request))
+	action = request.GET.get('action')
+	if not action:
+		return render_to_response('admin_settings.html',context_instance = RequestContext(request))
+	elif action == 'post':
+		return render_to_response('admin_post.html', context_instance = RequestContext(request))
+
 
 def post(request):
 	return render_to_response('admin_post.html',context_instance = RequestContext(request))
