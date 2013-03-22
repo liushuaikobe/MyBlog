b55fea68d17f8d37df0b4d9347e3069d4ee77b13
liushuaikobe <liushuaikobe@gmail.com>
2013-03-22 22:59:41 +0800

    init try to complete the admin-settings page's view, but not perfect

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'lskobe_blog',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'lskobe',                  # Not used with sqlite3.
+        'HOST': '219.217.227.89',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -121,6 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'browse',
+    'manage'
 )
 
 # A sample logging configuration. The only tangible logging
--- manage/views.py
+++ manage/views.py
@@ -2,15 +2,32 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from manage.models import kobe_meta
+
 def manage(request):
 	action = request.GET.get('action')
+	error = False
 	if not action:
-		return render_to_response('admin_settings.html',context_instance = RequestContext(request))
+		if request.method == 'GET':
+			return render_to_response('admin_settings.html', RequestContext(request))
+		else:
+			# print request.raw_post_data
+			if 'blogname' in request.POST and 'numofarticles' in request.POST:
+				blogname = request.POST['blogname']
+				numofarticles = request.POST['numofarticles']
+				if not blogname and not numofarticles:
+					return render_to_response('admin_settings.html', {'error':True}, RequestContext(request))
+				else:
+					kobe_meta.objects.filter(meta_key = 'blog_name').update(meta_value = blogname)
+					kobe_meta.objects.filter(meta_key = 'num_of_pages').update(meta_value = int(numofarticles))
+					return render_to_response('admin_settings.html', {'success':True}, RequestContext(request))
 	elif action == 'post':
-		return render_to_response('admin_post.html', context_instance = RequestContext(request))
+		return render_to_response('admin_post.html', RequestContext(request))
 	elif action == 'artmanage':
-		return render_to_response('admin_articlemanage.html', context_instance = RequestContext(request))
+		return render_to_response('admin_articlemanage.html', RequestContext(request))
+	elif action == 'catemanage':
+		return render_to_response('admin_catemanage.html', RequestContext(request))
 
 
 def post(request):
-	return render_to_response('admin_post.html',context_instance = RequestContext(request))
+	return render_to_response('admin_post.html', RequestContext(request))
