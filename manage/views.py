2dc15219baf86a28211842a816e0203268fec721
liushuaikobe <liushuaikobe@gmail.com>
2013-03-23 22:57:28 +0800

    complete admin_settings

--- manage/views.py
+++ manage/views.py
@@ -8,21 +8,30 @@ def manage(request):
 	action = request.GET.get('action')
 	error = False
 	if not action:
+		bn_queryset = kobe_meta.objects.filter(meta_key = 'blog_name')
+		np_queryset = kobe_meta.objects.filter(meta_key = 'num_of_pages')
+
+		meta = {'blog_name':bn_queryset[0].meta_value,
+				'num_of_pages':np_queryset[0].meta_value}
+
 		if request.method == 'GET':
-			return render_to_response('admin_settings.html', RequestContext(request))
+			return render_to_response('admin_settings.html', meta, RequestContext(request))
 		else:
-			# print request.raw_post_data
+			print request.raw_post_data
 			if 'blogname' in request.POST and 'numofarticles' in request.POST:
 				blogname = request.POST['blogname']
 				numofarticles = request.POST['numofarticles']
-				if not blogname and not numofarticles:
-					return render_to_response('admin_settings.html', {'error':'The data can not be null.' }, RequestContext(request))
+				if not blogname or not numofarticles:
+					meta['error'] = 'The data can not be null.'
+					return render_to_response('admin_settings.html', meta, RequestContext(request))
 				elif len(blogname) > 50:
-					return render_to_response('admin_settings.html', {'error':'The length of the blogname should less then 50.'}, RequestContext(request))
+					meta['error'] = 'The length of the blogname should less then 50.'
+					return render_to_response('admin_settings.html', meta, RequestContext(request))
 				else:
-					kobe_meta.objects.filter(meta_key = 'blog_name').update(meta_value = blogname)
-					kobe_meta.objects.filter(meta_key = 'num_of_pages').update(meta_value = int(numofarticles))
-					return render_to_response('admin_settings.html', {'success':True}, RequestContext(request))
+					bn_queryset.update(meta_value = blogname)
+					np_queryset.update(meta_value = int(numofarticles))
+					meta['success'] = True
+					return render_to_response('admin_settings.html', meta, RequestContext(request))
 	elif action == 'post':
 		return render_to_response('admin_post.html', RequestContext(request))
 	elif action == 'artmanage':
