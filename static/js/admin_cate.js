7f94111adca7279411d67605e91a4fd63e2905fc
liushuaikobe <liushuaikobe@gmail.com>
2013-03-24 22:46:12 +0800

    complete JS of adding the clicking event modify btn

---
+++ static/js/admin_cate.js
@@ -0,0 +1,20 @@
+$(function () {
+	$("div.cate").show();
+	$("div.cate_edit").hide();
+
+	$("button.btn-warning").click(function (e){
+		e.preventDefault();
+		var id = $(this).attr("id");
+		var dp_id = "dp_" + id.substr(6,id.length);
+		var ed_id = "ed_" + id.substr(6,id.length);
+		if ($("#" + dp_id).width() <= 0 || $("#" + dp_id).height() <= 0) {
+			$("#" + dp_id).show();
+			$("#" + ed_id).hide();
+		} else {
+			$("#" + dp_id).hide();
+			$("#" + ed_id).show()
+		}
+		return false;
+	})
+})
+
